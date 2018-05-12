7b5effde498a40bbbb502a4dc1ae5dbfe227ce41
Matthias Krüger <matthias.krueger@famsik.de>
2018-05-12 09:16:22 +0200

    try again

--- lib/tokenize.cpp
+++ lib/tokenize.cpp
@@ -212,7 +212,6 @@ bool Tokenizer::duplicateTypedef(Token **tokPtr, const Token *name, const Token 
     // check for an end of definition
     const Token * tok = *tokPtr;
     if (tok && Token::Match(tok->next(), ";|,|[|=|)|>|(|{")) {
-		
         const Token * end = tok->next();
 
         if (end->str() == "[") {
